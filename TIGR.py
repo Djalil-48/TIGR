dd183a67ee9a88992f1b5bb860f34863432d714b
Djalil <mkmohammed428@gmail.com>
2025-11-15 07:48:43 +0100

    TIGR.py
    Signed-off-by: Djalil  <mkmohammed428@gmail.com>

--- TIGR.py
+++ TIGR.py
@@ -1,112 +1,43 @@
-import os
-import time
-import sys
+hereimport os
 
-# شاشة تحميل وهمية مثل git clone
-os.system("clear")
-for i in range(1, 101):
-    sys.stdout.write(f"\rReceiving objects: {i}% ( {i}/100 ), 5.32 MiB | 1.{i} MiB/s")
-    sys.stdout.flush()
-    time.sleep(0.03)
+# Banner مثل الصورة
+banner = """
+\033[1;31m   d8888b.  d8888b. d8888b. 
+\033[1;32m   88  `8D  88  `8D 88  `8D 
+\033[1;33m   88oobY'  88oooY' 88oooY' 
+\033[1;34m   88`8b    88~~~b. 88~~~b. 
+\033[1;35m   88 `88.  88   8D 88   8D 
+\033[1;36m   88   YD  Y8888P' Y8888P' 
+\033[1;32m             TIGR TOOL  
+"""
 
-time.sleep(1)
-print("\nAlready up to date.\n")
-time.sleep(1)
+info = """
+\033[1;32m[-] AUTHOR   : DJALIL
+[-] TOOL     : TIGR
+[-] VERSION  : 1.0
+"""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-os.system("clear")
-
-# هنا تبدأ أداتك الأصلية
-print("""
-████████ TIGR ████████
-
-[1] START TOOL
-[2] ABOUT
+menu = """
+\033[1;31m[1] OPTION ONE
+[2] OPTION TWO
 [0] EXIT
-""")
+"""
 
-import os
-import time
+os.system("clear")
+print(banner)
+print(info)
+print(menu)
 
-# Colors
-R = "\033[1;31m"
-G = "\033[1;32m"
-Y = "\033[1;33m"
-B = "\033[1;34m"
-C = "\033[1;36m"
-W = "\033[1;37m"
+choose = input("\033[1;32m[-] CHOOSE : ")
 
-def banner():
-    os.system("clear")
-    print(R + """
-████████╗██╗ ██████╗ ██████╗ 
-╚══██╔══╝██║██╔════╝██╔═══██╗
-   ██║   ██║██║     ██║   ██║
-   ██║   ██║██║     ██║   ██║
-   ██║   ██║╚██████╗╚██████╔╝
-   ╚═╝   ╚═╝ ╚═════╝ ╚═════╝  
-""")
-    print(G + "[-] FACEBOOK : DJALIL mk")
-    print(G + "[-] TELEGRAM : djalil_dz_48")
-    print(G + "[-] AUTHOR   : DJALIL")
-    print(G + "[-] TOOL     : TIGR")
-    print(G + "[-] VERSION  : 1.0.0")
-    print(B + "----------------------------------------\n")
+if choose == "1":
+    print("\033[1;33mYOU SELECTED OPTION ONE")
 
-def menu():
-    print(G + "[1] FILE CLONING")
-    print(G + "[2] FOLLOW FB")
-    print(G + "[3] TOOL INFO")
-    print(R + "[0] EXIT\n")
-    return input(Y + "[-] CHOOSE : " + W)
+elif choose == "2":
+    print("\033[1;34mYOU SELECTED OPTION TWO")
 
-def file_cloning():
-    os.system("clear")
-    print(C + "[*] FILE CLONING MODULE COMING SOON...")
-    time.sleep(2)
+elif choose == "0":
+    print("\033[1;31mEXITING...")
 
-def follow_fb():
-    os.system("xdg-open https://facebook.com/")
-    print(G + "[*] OPENED FACEBOOK")
-    time.sleep(2)
-
-def tool_info():
-    os.system("clear")
-    print(G + "[*] TIGR TOOL CREATED BY DJALIL")
-    print(G + "[*] VERSION 1.0.0")
-    print(G + "[*] MADE WITH ❤️")
-    time.sleep(3)
-
-while True:
-    banner()
-    option = menu()
-
-    if option == "1":
-        file_cloning()
-
-    elif option == "2":
-        follow_fb()
-
-    elif option == "3":
-        tool_info()
-
-    elif option == "0":
-        print(R + "[!] EXITING...")
-        time.sleep(1)
-        break
-
-    else:
-        print(R + "[!] INVALID OPTION")
-        time.sleep(1)
+else:
+    print("\033[1;31mINVALID OPTION!")
